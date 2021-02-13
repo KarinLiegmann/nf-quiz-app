bcb1ef29adb56a61dee2d8b0e28994574b698144
Karin Liegmann <karin.liegmann@googlemail.com>
2021-02-13 19:28:29 +0100

    made renderCard-function in Cards.js

--- src/app.js
+++ src/app.js
@@ -1,48 +1,11 @@
 import { createElement } from './lib/createElement'
+
+import { renderCard } from './components/cards'
+
 import { createCardButton } from './components/cardButton'
-import { createCardQuestion } from './components/cardQuestion'
-
-
-createCards()
-
-function createCards() {
-
-    const cardContainer = document.querySelector('.card__container')
-    const cardContent = createElement('article', 'card')
-    cardContainer.appendChild(cardContent)
-
-
-
-    const questionContainer = createElement('div', 'test')
-    const questionHeadline = createElement('h2', 'card__title', 'fw-b', 'tt-uc')
-    questionHeadline.innerText = 'Question:'
-    questionContainer.appendChild(questionHeadline)
-
-    const cardQuestion = createCardQuestion('test')
-    questionContainer.appendChild(cardQuestion)
-
-    cardContent.appendChild(questionContainer)
-
-    // const bookmark = createElement('span', 'far fa-bookmark card__bookmark', cardContent)
+import { createCardQuestion, renderQuestion } from './components/cardQuestion'
 
 
 
 
-
-    const buttonContainer = createElement('div', 'btn__container')
-    questionContainer.appendChild(buttonContainer)
-    const cardButton = createCardButton('Show Answer')
-    buttonContainer.appendChild(cardButton)
-
-    const cardAnswer = createElement('div', 'card__answer')
-    cardAnswer.innerHTML = `<h2 class="card__title fw-b tt-uc">Answer:</h2>
-    <p>test</p>`
-    cardContent.appendChild(cardAnswer)
-
-    const cardTags = createElement('ul', 'tag__container')
-    cardTags.innerHTML = `<li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>`
-    cardContent.appendChild(cardTags)
-}
+renderCard('test', 'test')
---
+++ src/components/Cards/cards.js
@@ -0,0 +1,43 @@
+
+import { toggleAnswer } from './toggleanswer'
+import { toggleBookmark } from './bookmarks'
+
+function toggleElement(selector, toggleFunction) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(toggleFunction)
+
+    toggleElement('.card__button', toggleAnswer)
+    toggleElement('.card__bookmark', toggleBookmark)
+}
+
+import { createElement } from '../lib/createElement'
+
+const cardContainer = document.querySelector('.card__container')
+
+export function renderCard(question, answer) {
+
+    const cardContent = createElement('article', 'card')
+    cardContainer.appendChild(cardContent)
+
+    const questionContainer = createElement('div', 'card__question')
+    questionContainer.innerHTML = `<span class="far fa-bookmark card__bookmark"></span>
+    <h2 class="card__title fw-b tt-uc">Question:</h2>
+    <p class="card__question">${question}</p>`
+    cardContent.appendChild(questionContainer)
+
+    const buttonContainer = createElement('div', 'btn__container')
+    buttonContainer.innerHTML = `<button class="card__button btn cs-p">Show Answer</button>`
+    cardContent.appendChild(buttonContainer)
+
+    const cardAnswer = createElement('div', 'card__answer')
+    cardAnswer.innerHTML = `<h2 class="card__title fw-b tt-uc">Answer:</h2>
+    <p>${answer}</p>`
+    cardContent.appendChild(cardAnswer)
+
+    const cardTags = createElement('ul', 'tag__container')
+    cardTags.innerHTML = `<li>Test</li>
+    <li>Test</li>
+    <li>Test</li>
+    <li>Test</li>`
+    cardContent.appendChild(cardTags)
+}
